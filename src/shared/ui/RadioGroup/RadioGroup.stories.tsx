9c874c09b7a072122a5d8ea5738070a2a54555ef
belovroman90 <belov.roman.90@mail.ru>
2025-08-20 20:50:18 +0700

    Created RadioButton directory and set up storybook

---
+++ src/shared/ui/RadioGroup/RadioGroup.stories.tsx
@@ -0,0 +1,10 @@
+import { RadioGroup } from './RadioGroup';
+
+export default {
+  title: 'shared/ui/RadioGroup',
+  component: RadioGroup,
+};
+
+export const Default = () => {
+  return <RadioGroup />;
+};
---
+++ src/shared/ui/RadioGroup/RadioGroup.tsx
@@ -0,0 +1,11 @@
+import './RadioGroup.scss';
+
+export const RadioGroup = () => {
+  return (
+    <div>
+      <label>
+        <input type="radio" value="some value" checked="unchecked" />
+      </label>
+    </div>
+  );
+};
