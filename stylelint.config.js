65a0559b2ee95c3b522ff5fdcc21719272498ead
alekcandr1 <alekcandrmain@gmail.com>
2025-08-21 10:17:10 +0300

    feat(typography): add font map with text styles and basic tags

--- stylelint.config.js
+++ stylelint.config.js
@@ -2,30 +2,21 @@ module.exports = {
   extends: ['stylelint-config-standard', 'stylelint-config-standard-scss'],
   plugins: ['stylelint-scss'],
   rules: {
+    'comment-empty-line-before': null,
     'property-no-unknown': null,
     'selector-pseudo-class-no-unknown': [
       true,
       {
-        ignorePseudoClasses: ['export'],
-      },
+        ignorePseudoClasses: ['export']
+      }
     ],
     'at-rule-no-unknown': [
       true,
       {
-        ignoreAtRules: [
-          'use',
-          'forward',
-          'mixin',
-          'include',
-          'extend',
-          'each',
-          'for',
-          'if',
-          'else',
-        ],
-      },
+        ignoreAtRules: ['use', 'forward', 'mixin', 'include', 'extend', 'each', 'for', 'if', 'else']
+      }
     ],
     'no-duplicate-selectors': null,
-    'no-descending-specificity': null,
-  },
+    'no-descending-specificity': null
+  }
 };
--- vitest.config.ts
+++ vitest.config.ts
@@ -5,8 +5,7 @@ import { defineConfig } from 'vitest/config';
 
 import { storybookTest } from '@storybook/addon-vitest/vitest-plugin';
 
-const dirname =
-  typeof __dirname !== 'undefined' ? __dirname : path.dirname(fileURLToPath(import.meta.url));
+const dirname = typeof __dirname !== 'undefined' ? __dirname : path.dirname(fileURLToPath(import.meta.url));
 
 // More info at: https://storybook.js.org/docs/next/writing-tests/integrations/vitest-addon
 export default defineConfig({
@@ -17,7 +16,7 @@ export default defineConfig({
         plugins: [
           // The plugin will run tests for the stories defined in your Storybook config
           // See options at: https://storybook.js.org/docs/next/writing-tests/integrations/vitest-addon#storybooktest
-          storybookTest({ configDir: path.join(dirname, '.storybook') }),
+          storybookTest({ configDir: path.join(dirname, '.storybook') })
         ],
         test: {
           name: 'storybook',
@@ -25,11 +24,11 @@ export default defineConfig({
             enabled: true,
             headless: true,
             provider: 'playwright',
-            instances: [{ browser: 'chromium' }],
+            instances: [{ browser: 'chromium' }]
           },
-          setupFiles: ['.storybook/vitest.setup.ts'],
-        },
-      },
-    ],
-  },
+          setupFiles: ['.storybook/vitest.setup.ts']
+        }
+      }
+    ]
+  }
 });
