c285eed0abf26621d8be4952d2c00c05637ba816
Irina Balyuk <ivbalyuk@gmail.com>
2025-08-21 22:24:09 +0300

    create checkbox, add to SB and settings SB

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,18 +1,17 @@
 import type { StorybookConfig } from '@storybook/nextjs-vite';
 
 const config: StorybookConfig = {
-  stories: ['../stories/**/*.mdx', '../stories/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
+  stories: [
+    '../src/shared/ui/**/*.stories.@(js|jsx|ts|tsx)', // 👈 Правильный путь!
+  ],
   addons: [
-    '@chromatic-com/storybook',
-    '@storybook/addon-docs',
-    '@storybook/addon-onboarding',
-    '@storybook/addon-a11y',
-    '@storybook/addon-vitest',
+    // '@storybook/addon-essentials',
+    // '@storybook/addon-styling',
   ],
   framework: {
     name: '@storybook/nextjs-vite',
     options: {},
   },
-  staticDirs: ['..\\public'],
+  staticDirs: ['../public'],
 };
 export default config;
--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,13 @@
 import styles from './page.module.css';
+import { Checkbox } from '@/src/shared/ui/Checkbox/Checkbox';
 
 export default function Home() {
   return (
     <div className={styles.page}>
       <div>VOPP.ME</div>
-      <main className={styles.main}></main>
+      <main className={styles.main}>
+        <Checkbox />
+      </main>
       <footer className={styles.footer}></footer>
     </div>
   );
