d8d26310c6847129e5290cdbdeff5b6914e949b9
Mikhail8815 <mishutka150588@mail.ru>
2025-08-25 20:38:01 +0300

    IN-17 refactor: add jsdoc description

--- src/components/ui/Textarea.tsx
+++ src/components/ui/Textarea.tsx
@@ -10,6 +10,31 @@ type TextareaProps = {
   resize?: 'none' | 'both' | 'horizontal' | 'vertical';
 } & React.TextareaHTMLAttributes<HTMLTextAreaElement>;
 
+/**
+ * Кастомное текстовое поле с расширенными возможностями стилизации.
+ *
+ * @param {Object} props - Свойства компонента.
+ * @param {string} [props.label] - Текст метки над полем ввода.
+ * @param {string} [props.placeholder='Enter text'] - Текст-заполнитель.
+ * @param {boolean} [props.disabled=false] - Отключено ли поле.
+ * @param {string} [props.errorMessage] - Сообщение об ошибке (если есть, поле стилизуется как ошибочное).
+ * @param {'none' | 'both' | 'horizontal' | 'vertical'} [props.resize='vertical'] - Направление изменения размера.
+ * @param {React.Ref<HTMLTextAreaElement>} ref - Ref для доступа к нативному textarea элементу.
+ * @returns {React.ReactElement} Кастомный текстовый компонент.
+ *
+ * @example
+ * // Базовое использование
+ * <Textarea label="Описание" placeholder="Введите текст" />
+ *
+ * @example
+ * // С отключением resize и сообщением об ошибке
+ * <Textarea
+ *   label="Комментарий"
+ *   resize="none"
+ *   errorMessage="Поле обязательно для заполнения!"
+ * />
+ */
+
 export const Textarea = React.forwardRef<HTMLTextAreaElement, TextareaProps>(
   (
     { label, placeholder = 'Enter text', disabled = false, errorMessage, resize = 'vertical', className, ...props },
