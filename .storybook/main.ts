bcd9591d5bb1281fc5109fb08957f82c39e8122c
asyakz <asyakz@gmail.com>
2025-08-26 23:37:50 +0500

    Create Button component, scss file, stories file

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,7 +1,7 @@
 import type { StorybookConfig } from '@storybook/nextjs-vite';
 
 const config: StorybookConfig = {
-  stories: ['../stories/**/*.mdx', '../stories/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
+  stories: ['../src/shared/ui/**/*.stories.@(js|jsx|ts|tsx)'],
   addons: [
     '@chromatic-com/storybook',
     '@storybook/addon-docs',
@@ -13,6 +13,6 @@ const config: StorybookConfig = {
     name: '@storybook/nextjs-vite',
     options: {}
   },
-  staticDirs: ['..\\public']
+  staticDirs: ['../public']
 };
 export default config;
