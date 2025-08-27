f01dd424c8ed5f89c4ea4f855e88fd52819cdb11
Marina Vyatkina <marinavyatk74@mail.ru>
2025-08-28 00:10:41 +0500

    IN-32 fix: svg issue

--- next.config.ts
+++ next.config.ts
@@ -1,15 +1,37 @@
 import type { NextConfig } from 'next';
+import type { RuleSetRule } from 'webpack';
 
 const nextConfig: NextConfig = {
-  // images: {
-  //     remotePatterns: [
-  //         {
-  //             protocol: 'https',
-  //             hostname: '',
-  //             pathname: ''
-  //         }
-  //     ]
-  // }
+  webpack(config) {
+    const fileLoaderRule = config.module.rules.find(
+      (rule: RuleSetRule | string): rule is RuleSetRule =>
+        typeof rule !== 'string' && rule.test instanceof RegExp && rule.test.test('.svg')
+    );
+
+    if (!fileLoaderRule) {
+      throw new Error('SVG file loader rule not found');
+    }
+
+    config.module.rules.push(
+      {
+        ...fileLoaderRule,
+        test: /\.svg$/i,
+        resourceQuery: /url/
+      },
+      {
+        test: /\.svg$/i,
+        issuer: fileLoaderRule.issuer,
+        resourceQuery: {
+          not: [...((fileLoaderRule.resourceQuery as { not?: RegExp[] })?.not || []), /url/]
+        },
+        use: ['@svgr/webpack']
+      }
+    );
+
+    fileLoaderRule.exclude = /\.svg$/i;
+
+    return config;
+  }
 };
 
 export default nextConfig;
