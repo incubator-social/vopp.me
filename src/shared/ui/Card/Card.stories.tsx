e52d092d90c1e63ca4c8f6868ed02544c7d58119
belovroman90 <belov.roman.90@mail.ru>
2025-08-29 07:50:06 +0700

    Создал Card, стили и storybook

--- src/shared/ui/Card/Card.stories.tsx
+++ src/shared/ui/Card/Card.stories.tsx
@@ -1,4 +1,6 @@
-import Card from './Card';
+import { Meta, StoryObj } from '@storybook/nextjs-vite';
+import styles from './Card.module.scss';
+import Card, { CardProps } from './Card';
 
 const meta = {
   title: 'UI/Card',
@@ -12,15 +14,40 @@ const meta = {
       }
     }
   }
-} satisfies Meta<typeof RadioGroup>;
+} satisfies Meta<typeof Card>;
 
 export default meta;
 type Story = StoryObj<typeof meta>;
 
-const Template: Story['render'] = () => {
-  return <Card />;
+const Template: Story['render'] = (args: CardProps) => {
+  return <Card className={args.className}>{args.children}</Card>;
 };
 
 export const Default: Story = {
-  render: Template
+  render: Template,
+  args: {
+    children: <div>Default</div>
+  }
+};
+
+export const Signup: Story = {
+  render: Template,
+  args: {
+    className: styles['sign-up'],
+    children: (
+      <div className={styles.container}>
+        <h1>Sign Up</h1>
+        <div className={styles.icons}></div>
+        <div className={styles.form}></div>
+        <div className={styles.terms}></div>
+        <button className={styles.button}>
+          <h3>Sign Up</h3>
+        </button>
+        <p className={styles['account-text']}>Do you have an account?</p>
+        <span className={styles['sign-in']}>
+          <a className={styles['sign-in-link']}>Sign In</a>
+        </span>
+      </div>
+    )
+  }
 };
--- src/shared/ui/Card/Card.tsx
+++ src/shared/ui/Card/Card.tsx
@@ -1,13 +1,18 @@
+'use client';
+
+import clsx from 'clsx';
 import { ReactNode } from 'react';
 import styles from './Card.module.scss';
 
-const Card = ({ children, className }) => {
-  return <div className={className || styles.card}>{children}</div>;
+const Card = ({ children, className }: CardProps) => {
+  const combinedClassName = clsx(styles.card, className);
+  return <div className={combinedClassName}>{children}</div>;
 };
 
 export default Card;
 
-type CardProps = {
+//types
+export type CardProps = {
   children: ReactNode;
   className?: string;
 };
