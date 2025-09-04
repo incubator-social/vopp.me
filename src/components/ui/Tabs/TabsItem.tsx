81871081bf699d0ec0b7b52c8b4cfbf4962389d2
alekcandr1 <alekcandrmain@gmail.com>
2025-09-04 10:33:52 +0300

    fix for Merge

--- src/components/ui/Tabs/TabsItem.tsx
+++
@@ -1,6 +0,0 @@
-import * as RadixTabs from '@radix-ui/react-tabs';
-import { TabsContentProps } from '@radix-ui/react-tabs';
-
-export const TabsItem = (props: TabsContentProps) => {
-  return <RadixTabs.Content {...props} />;
-};
