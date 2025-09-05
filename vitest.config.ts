47d7118ac7ff880e5682c61e3b11dd3b3a87939a
Aleksandr Bokhan <80443875+alekcandr1@users.noreply.github.com>
2025-09-05 13:58:22 +0300

    Update vitest.config.ts
    commented string №6

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,7 +3,7 @@ import { fileURLToPath } from 'node:url';
 
 import { defineConfig } from 'vitest/config';
 
-const dirname = typeof __dirname !== 'undefined' ? __dirname : path.dirname(fileURLToPath(import.meta.url));
+// const dirname = typeof __dirname !== 'undefined' ? __dirname : path.dirname(fileURLToPath(import.meta.url));
 
 // More info at: https://storybook.js.org/docs/next/writing-tests/integrations/vitest-addon
 export default defineConfig({
