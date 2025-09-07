44524d5ff67744c25f5d780f3fa5f9047e285dbe
Irina Balyuk <ivbalyuk@gmail.com>
2025-09-07 10:03:03 +0300

    Fixed scroll

--- src/shared/ui/Scroll/Scroll.tsx
+++ src/shared/ui/Scroll/Scroll.tsx
@@ -4,10 +4,12 @@ import * as ScrollArea from '@radix-ui/react-scroll-area';
 import { ReactNode } from 'react';
 import styles from './Scroll.module.scss';
 
-const Scroll = ({ children, type }: ScrollAreaProps) => {
+const Scroll = ({ children, type = ScrollType.auto, viewportAsChild = false }: ScrollAreaProps) => {
   return (
-    <ScrollArea.Root className={styles.root} type={type ? type : ScrollType.always}>
-      <ScrollArea.Viewport className={styles.viewport}>{children}</ScrollArea.Viewport>
+    <ScrollArea.Root className={styles.root} type={type}>
+      <ScrollArea.Viewport className={styles.viewport} {...(viewportAsChild ? { asChild: true } : {})}>
+        {children}
+      </ScrollArea.Viewport>
       <ScrollArea.Scrollbar className={styles['scroll-bar']} orientation={'horizontal'}>
         <ScrollArea.Thumb className={styles.thumb} />
       </ScrollArea.Scrollbar>
@@ -24,9 +26,10 @@ export default Scroll;
 export type ScrollAreaProps = {
   children: ReactNode;
   type?: ScrollType;
+  viewportAsChild?: boolean;
 };
 
-enum ScrollType {
+export enum ScrollType {
   auto = 'auto',
   always = 'always',
   scroll = 'scroll',
