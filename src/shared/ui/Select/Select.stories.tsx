98e8f327eb611d3a0adcab98af555fc07f864df1
asyakz <asyakz@gmail.com>
2025-09-09 20:08:11 +0500

    IN-14 fix-6: Select.stories.tsx

--- src/shared/ui/Select/Select.stories.tsx
+++ src/shared/ui/Select/Select.stories.tsx
@@ -1,5 +1,5 @@
-'use client';
-import { Meta, StoryObj } from '@storybook/nextjs-vite';
+import React, { useState } from 'react';
+import { Meta } from '@storybook/nextjs-vite';
 import { Select } from './Select';
 import IconFlagRussia from './../../assets/icons/flag-russia.svg';
 import IconFlagUnitedKingdom from './../../assets/icons/flag-united-kingdom.svg';
@@ -68,7 +68,6 @@ const [selectedValue, setSelectedValue] = useState('');
 } satisfies Meta<typeof Select>;
 
 export default meta;
-type Story = StoryObj<typeof meta>;
 
 const defaultOptions = [
   { value: '1', label: 'Option 1' },
@@ -83,7 +82,7 @@ const defaultOptions = [
   { value: '10', label: 'Option 10' }
 ];
 
-const onlyNumber = [
+const onlyNumbers = [
   { value: '1', label: '10' },
   { value: '2', label: '20' },
   { value: '3', label: '30' },
@@ -109,163 +108,236 @@ const longOptionsArr = [
   { value: '3', label: 'Короткое' }
 ];
 
-const change = (value: string) => console.log('Selected:', value);
+// Default
 
-export const Default: Story = {
-  args: {
-    options: defaultOptions,
-    value: '1',
-    placeholder: 'Выберите опцию...',
-    onValueChange: change
-  },
-  parameters: {
-    docs: {
-      description: {
-        story: `Стандартный селект с предвыбранным значением`
-      }
+export const Default = () => {
+  const [selected, setSelected] = useState('1');
+
+  return (
+    <div>
+      <Select options={defaultOptions} value={selected} onValueChange={setSelected} />
+    </div>
+  );
+};
+
+Default.parameters = {
+  docs: {
+    description: {
+      story: `Стандартный селект с предвыбранным значением`
     }
   }
 };
 
-export const NumbersOptions: Story = {
-  args: {
-    options: onlyNumber,
-    value: '1',
-    placeholder: 'Выберите опцию...',
-    onValueChange: change,
-    size: {
-      minWidth: 52,
-      minHeight: 24,
-      maxHeight: 24,
-      arrowSize: 16,
-      padding: '0 8px',
-      fontSize: 14
-    },
-    contentWidth: 'trigger'
-  },
-  parameters: {
-    docs: {
-      description: {
-        story: `Компактный селект с числовыми значениями`
-      }
+// Только числа
+
+export const NumbersOptions = () => {
+  const [selected, setSelected] = useState('3');
+
+  const selectSize = {
+    minWidth: 52,
+    minHeight: 24,
+    maxHeight: 24,
+    arrowSize: 16,
+    padding: '0 8px',
+    fontSize: 14
+  };
+  return (
+    <div>
+      <Select
+        options={onlyNumbers}
+        size={selectSize}
+        contentWidth={'trigger'}
+        value={selected}
+        onValueChange={setSelected}
+      />
+    </div>
+  );
+};
+
+NumbersOptions.parameters = {
+  docs: {
+    description: {
+      story: `Компактный селект с числовыми значениями`
     }
   }
 };
 
-export const DisabledSelect: Story = {
-  args: {
-    options: defaultOptions,
-    value: '1',
-    placeholder: 'Select is disabled',
-    disabled: true,
-    onValueChange: change
-  },
-  parameters: {
-    docs: {
-      description: {
-        story: `Заблокированный селект`
-      }
+// Disabled
+
+export const DisabledSelect = () => {
+  const [selected, setSelected] = useState('');
+
+  const selectSize = {
+    minWidth: 52,
+    minHeight: 24,
+    maxHeight: 24,
+    arrowSize: 16,
+    padding: '0 8px',
+    fontSize: 14
+  };
+  return (
+    <div>
+      <Select
+        disabled={true}
+        options={defaultOptions}
+        size={selectSize}
+        placeholder={'Select is disabled'}
+        value={selected}
+        onValueChange={setSelected}
+      />
+    </div>
+  );
+};
+
+DisabledSelect.parameters = {
+  docs: {
+    description: {
+      story: `Заблокированный селект`
     }
   }
 };
 
-export const EmptyOptions: Story = {
-  args: {
-    options: [],
-    value: '1',
-    placeholder: 'No options available...',
-    onValueChange: change
-  },
-  parameters: {
-    docs: {
-      description: {
-        story: `Селект без доступных опций`
-      }
+// Пустой массив опций
+
+export const EmptyOptions = () => {
+  const [selected, setSelected] = useState('');
+
+  return (
+    <div>
+      <Select
+        options={defaultOptions}
+        placeholder={'No options available...'}
+        value={selected}
+        onValueChange={setSelected}
+      />
+    </div>
+  );
+};
+
+EmptyOptions.parameters = {
+  docs: {
+    description: {
+      story: `Селект без доступных опций`
     }
   }
 };
 
-export const WithIcons: Story = {
-  args: {
-    options: optionsWithIcons,
-    value: '1',
-    placeholder: 'Выберите страну...',
-    onValueChange: change,
-    contentWidth: 'trigger',
-    size: {
-      maxWidth: 240,
-      minWidth: 240
-    }
-  },
-  parameters: {
-    docs: {
-      description: {
-        story: `Селект с иконками и текстом`
-      }
+// C иконками и текстом
+
+export const WithIcons = () => {
+  const [selected, setSelected] = useState('');
+
+  return (
+    <div>
+      <Select
+        options={optionsWithIcons}
+        size={{ maxWidth: 240, minWidth: 240 }}
+        placeholder={'Выберите страну...'}
+        value={selected}
+        contentWidth={'trigger'}
+        onValueChange={setSelected}
+      />
+    </div>
+  );
+};
+
+WithIcons.parameters = {
+  docs: {
+    description: {
+      story: `Селект с иконками и текстом`
     }
   }
 };
 
-export const CustomSizes: Story = {
-  args: {
-    options: defaultOptions,
-    value: '1',
-    placeholder: 'Custom sizes...',
-    onValueChange: change,
-    size: {
-      padding: '3px',
-      arrowSize: 50,
-      minHeight: 17,
-      maxHeight: 24
-    }
-  },
-  parameters: {
-    docs: {
-      description: {
-        story: `Селект с кастомными размерами`
-      }
+// Кастомные размеры
+
+export const CustomSizes = () => {
+  const [selected, setSelected] = useState('');
+
+  const selectSize = {
+    padding: '20px',
+    arrowSize: 50,
+    minHeight: 17,
+    maxHeight: 24,
+    fontSize: 30
+  };
+
+  return (
+    <div>
+      <Select
+        options={defaultOptions}
+        size={selectSize}
+        placeholder={'Custom sizes...'}
+        value={selected}
+        onValueChange={setSelected}
+      />
+    </div>
+  );
+};
+
+CustomSizes.parameters = {
+  docs: {
+    description: {
+      story: `Селект с кастомными размерами`
     }
   }
 };
 
-export const OnlyIcon: Story = {
-  args: {
-    options: optionsWithIconsOnly,
-    value: '1',
-    placeholder: 'Выберите иконку',
-    onValueChange: change,
-    size: {
-      iconSize: 40,
-      arrowSize: 40,
-      minWidth: 50
-    }
-  },
-  parameters: {
-    docs: {
-      description: {
-        story: `Селект только с иконками`
-      }
+// Только иконки
+
+export const OnlyIcon = () => {
+  const [selected, setSelected] = useState('');
+
+  const selectSize = {
+    iconSize: 40,
+    arrowSize: 40,
+    minWidth: 50
+  };
+
+  return (
+    <div>
+      <Select
+        options={optionsWithIconsOnly}
+        size={selectSize}
+        placeholder={'Выберите иконку'}
+        value={selected}
+        onValueChange={setSelected}
+      />
+    </div>
+  );
+};
+
+OnlyIcon.parameters = {
+  docs: {
+    description: {
+      story: `Селект только с иконками`
     }
   }
 };
 
-export const LongOptions: Story = {
-  args: {
-    options: longOptionsArr,
-    value: '1',
-    placeholder: 'Выбрать опцию...',
-    onValueChange: change,
-    size: {
-      maxWidth: 250,
-      minWidth: 250
-    },
-    contentWidth: 'content'
-  },
-  parameters: {
-    docs: {
-      description: {
-        story: `Селект с длинными текстовыми опциями`
-      }
+// Длинные и короткие опции
+
+export const LongOptions = () => {
+  const [selected, setSelected] = useState('');
+
+  return (
+    <div>
+      <Select
+        options={longOptionsArr}
+        size={{ maxWidth: 250, minWidth: 250 }}
+        contentWidth={'content'}
+        placeholder={'Выбрать опцию...'}
+        value={selected}
+        onValueChange={setSelected}
+      />
+    </div>
+  );
+};
+
+LongOptions.parameters = {
+  docs: {
+    description: {
+      story: `Селект с длинными и короткими текстовыми опциями`
     }
   }
 };
