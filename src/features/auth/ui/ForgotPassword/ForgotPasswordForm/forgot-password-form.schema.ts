ef3b8b48ec6358644fa9e67bf9d383969e233a62
alekcandr1 <alekcandrmain@gmail.com>
2025-09-18 09:33:39 +0300

    IN-40 fix: debug type ForgotPasswordFormValues

--- src/features/auth/ui/ForgotPassword/ForgotPasswordForm/forgot-password-form.schema.ts
+++ src/features/auth/ui/ForgotPassword/ForgotPasswordForm/forgot-password-form.schema.ts
@@ -1,10 +1,8 @@
 import { z } from 'zod';
 import { emailSchema } from '@/src/shared/schemas/form.shema';
 
-export type ForgotPasswordFormValues = {
-  email: string;
-};
-
 export const forgotPasswordSchema = z.object({
   email: emailSchema
 });
+
+export type ForgotPasswordFormValues = z.infer<typeof forgotPasswordSchema>;
