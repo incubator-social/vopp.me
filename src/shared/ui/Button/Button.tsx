05f85f9d0d5a616b9f1c5c62bb8494f38c0b6bf7
asyakz <asyakz@gmail.com>
2025-09-19 00:53:42 +0500

    IN-16-fix: Button fixed

--- src/shared/ui/Button/Button.tsx
+++ src/shared/ui/Button/Button.tsx
@@ -27,7 +27,6 @@ type Props = {
   type?: 'button' | 'submit' | 'reset';
   href?: string;
   target?: '_self' | '_blank';
-  asChild?: boolean;
 };
 
 export const Button = ({
@@ -38,8 +37,7 @@ export const Button = ({
   size = {},
   type = 'button',
   href,
-  target = '_self',
-  asChild = false
+  target = '_self'
 }: Props) => {
   const buttonStyles: CSSProperties = {
     minWidth: size?.minWidth,
@@ -50,26 +48,6 @@ export const Button = ({
     height: size?.height,
     padding: size?.padding
   };
-
-  // Если asChild=true, компонент должен не создавать собственный DOM-элемент, а применить свои стили и поведение к дочернему элементу.
-  if (asChild) {
-    // Проверка, является ли children валидным React-элементом (какие пропсы ожидаются у дочернего элемента)
-    if (isValidElement<InjectedButtonProps>(children)) {
-      const child = children as ReactElement<InjectedButtonProps>;
-      // Функция cloneElement из React используется для создания копии существующего React-элемента с новыми или измененными пропсами
-      return cloneElement(child, {
-        // Клонируем элемент с новыми пропсами
-        className: clsx(styles.button, styles[variant]),
-        style: { ...buttonStyles, ...child.props.style },
-        onClick: (e: React.MouseEvent) => {
-          onClick?.();
-          child.props.onClick?.(e);
-        }
-      });
-    }
-    // Если дочерний элемент невалиден, он возвращается как есть
-    return <>{children}</>;
-  }
 
   // Если передан href, рендерим как ссылку
   if (href) {
