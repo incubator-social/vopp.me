3a0c81e3434d261e90a86aabc9e0e070b8edab85
alekcandr1 <alekcandrmain@gmail.com>
2025-09-24 11:50:15 +0300

    IN-53 feat: add handleError.ts

---
+++ src/shared/lib/utils/handleError.ts
@@ -0,0 +1,23 @@
+import { BaseQueryApi, FetchBaseQueryError, FetchBaseQueryMeta, QueryReturnValue } from '@reduxjs/toolkit/query';
+
+export const handleError = (
+  api: BaseQueryApi,
+  result: QueryReturnValue<unknown, FetchBaseQueryError, FetchBaseQueryMeta>
+) => {
+  let error = 'Some error occurred';
+
+  if (result.error) {
+    switch (result.error.status) {
+      case 'FETCH_ERROR':
+        error = 'Network error. Please check your connection.';
+        break;
+      case 400:
+      case 500:
+        error = (result.error.data as { message?: string })?.message || 'Server error';
+        break;
+      default:
+        error = JSON.stringify(result.error);
+    }
+    api.dispatch(setAppError({ error }));
+  }
+};
