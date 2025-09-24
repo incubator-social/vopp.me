392f06c870246d4f5b17c476be9435b96025c62d
alekcandr1 <alekcandrmain@gmail.com>
2025-09-24 14:00:34 +0300

    IN-53 feat: change handleError.ts

--- src/shared/lib/utils/handleError.ts
+++ src/shared/lib/utils/handleError.ts
@@ -1,24 +1,30 @@
 import { BaseQueryApi, FetchBaseQueryError, FetchBaseQueryMeta, QueryReturnValue } from '@reduxjs/toolkit/query';
 import { setAppError } from '@/app/appSlice';
 import { isErrorWithMessage } from './isErrorWithMessage';
+import { ResponseErrorType } from '@/src/shared/types/api';
 
 export const handleError = (
   api: BaseQueryApi,
   result: QueryReturnValue<unknown, FetchBaseQueryError, FetchBaseQueryMeta>
 ) => {
   let error = 'Some error occurred';
+  const data = result.error?.data as ResponseErrorType | undefined;
 
   if (result.error) {
     switch (result.error.status) {
+      case 'PARSING_ERROR':
+      case 'CUSTOM_ERROR':
+        error = result.error.error;
+        break;
       case 'FETCH_ERROR':
         error = 'Network error. Please check your connection.';
         break;
       case 400:
       case 500:
-        if (isErrorWithMessage(result.error.data)) {
-          error = result.error.data.message;
+        if (isErrorWithMessage(data?.messages[0])) {
+          error = data.messages[0].message;
         } else {
-          error = JSON.stringify(result.error.data);
+          error = JSON.stringify(data);
         }
         break;
       default:
