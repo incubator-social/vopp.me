3c246563cae3efc25cf75cb6d7a7414ce6e1b253
alekcandr1 <alekcandrmain@gmail.com>
2025-10-05 14:29:30 +0300

    fix: change API url

--- src/shared/api/baseQuery.ts
+++ src/shared/api/baseQuery.ts
@@ -2,7 +2,7 @@ import { fetchBaseQuery } from '@reduxjs/toolkit/query';
 import { AUTH_KEYS } from '../config/storage';
 
 export const baseQuery = fetchBaseQuery({
-  baseUrl: 'https://inctagram.work/api/v1',
+  baseUrl: 'https://vopp.me/api/v1',
   credentials: 'include',
   prepareHeaders: (headers) => {
     const token = typeof window !== 'undefined' ? localStorage.getItem(AUTH_KEYS.accessToken) : null;
