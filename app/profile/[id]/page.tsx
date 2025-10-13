06025e1d119c1a243948e5f848263e203fb1d736
alekcandr1 <alekcandrmain@gmail.com>
2025-10-13 12:19:08 +0300

    IN-60-4 feat

--- app/profile/[id]/page.tsx
+++ app/profile/[id]/page.tsx
@@ -4,17 +4,23 @@ import { useParams } from 'next/navigation';
 import Link from 'next/link';
 import { Button } from '@/src/shared/ui/Button/Button';
 import { ROUTES } from '@/src/shared/config/routes';
+import { useAuth } from '@/src/features/auth/lib/useAuth';
 
 export default function UserProfilePage() {
   const params = useParams<{ id: string }>();
   const userId = params.id;
+  const { user } = useAuth();
+  const isMyProfile = user?.userId === Number(params.id);
 
   return (
     <div style={{ padding: 20 }}>
       <h1>Профиль пользователя {userId}</h1>
-      <Button variant={'buttonSecondary'} asChild={true}>
-        <Link href={ROUTES.SETTINGS}>Profile Settings</Link>
-      </Button>
+
+      {isMyProfile && (
+        <Button variant={'buttonSecondary'} asChild={true}>
+          <Link href={ROUTES.SETTINGS}>Profile Settings</Link>
+        </Button>
+      )}
     </div>
   );
 }
